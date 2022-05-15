0279b5efb3ee7cbd30d76ae564b5575a7d4644c6
Sultan <godsultan42@gmail.com>
2022-05-15 22:52:19 +0200

    feature: bot can join and play music framework: discord-player

--- src/index.js
+++ src/index.js
@@ -2,25 +2,59 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const { Client, Intents } = require('discord.js');
-const { joinVoiceChannel } = require('@discordjs/voice');
+const { Player } = require('discord-player');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES] });
+const player = new Player(client);
+
+player.on("trackStart", (queue, track) => queue.metadata.channel.send(`Now playing ${track.title}`));
 
 client.once("ready", () => {
     console.log("We are ready");
 });
 
-client.on('message', async message => {
+client.on('messageCreate', async message => {
     if (message.content === '-ping') {
         message.reply(`${message.member} poshel naxuy!`);
         return;
-    } else if (message.content === '-play') {
-        const channel = message.member.voice.channel;
-        const connection = joinVoiceChannel({
-            channelId: channel.id,
-            guildId: channel.guild.id,
-            adapterCreator: channel.guild.voiceAdapterCreator
+    } else if (message.content.startsWith('-play')) {
+        //Check who sent the message if he in VC
+        if (!message.member.voice.channelId) return await message.reply(`${message.member} zaydi v golosovoe ༼ つ ◕_◕ ༽つ`);
+        //Check if bot on other channel
+        if (message.guild.me.voice.channelId && message.member.voice.channelId !== message.guild.me.voice.channelId) {
+            return await message.reply(`${message.member} ya ne s toboy v kanale ¯\_(ツ)_/¯`);
+        }
+
+        //Name of song
+        const query = message.content.split(' ').slice(1).join(' ');
+        //Check who sent the message if he wrote name of song after command
+        if (!query) return message.reply(`${message.member} dai nazvanie pesni ಠ_ಠ`);
+        
+
+        const queue = player.createQueue(message.guild, {
+            metadata: {
+                channel: message.channel
+            }
         });
+
+        //Connecting to VC
+        try {
+            if (!queue.connection) await queue.connect(message.member.voice.channel);
+        } catch {
+            queue.destroy();
+            return await message.reply(`Ne poluchilos' podrubitsya`);
+        }
+
+        //Try to find song
+        const track = await player.search(query, {
+            requestedBy: message.member.user
+        }).then(x => x.tracks[0]);
+
+        if (!track) return message.reply(`${query}, ne nashli track (ಥ﹏ಥ)`);
+
+        queue.play(track);
+
+        return await message.reply("priyatnogo proslushivaniya ヾ(⌐■_■)ノ♪");
     }
 });
 
